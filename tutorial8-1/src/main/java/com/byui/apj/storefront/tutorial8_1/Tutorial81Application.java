23c19aa5ea83935294c3dfddd857bfc030910cd0
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-02-24 12:30:41 -0700

    tutorial8-1 init

---
+++ tutorial8-1/src/main/java/com/byui/apj/storefront/tutorial8_1/Tutorial81Application.java
@@ -0,0 +1,13 @@
+package com.byui.apj.storefront.tutorial8_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial81Application.class, args);
+	}
+
+}
---
+++ tutorial8-1/src/test/java/com/byui/apj/storefront/tutorial8_1/Tutorial81ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byui.apj.storefront.tutorial8_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial81ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
