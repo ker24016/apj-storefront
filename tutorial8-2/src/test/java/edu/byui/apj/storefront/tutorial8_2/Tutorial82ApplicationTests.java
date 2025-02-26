1e8d1500fe020490ce14810aeb75209d307422da
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-02-26 14:50:23 -0700

    com -> edu

---
+++ tutorial8-2/src/test/java/edu/byui/apj/storefront/tutorial8_2/Tutorial82ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial8_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial82ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
