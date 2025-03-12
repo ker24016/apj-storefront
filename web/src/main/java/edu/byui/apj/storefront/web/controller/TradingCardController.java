b40fa1f37092d41eba11d7f810fd8759ec7a126b
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-12 14:46:26 -0600

    Week 9 working

--- web/src/main/java/edu/byui/apj/storefront/web/controller/TradingCardController.java
+++ web/src/main/java/edu/byui/apj/storefront/web/controller/TradingCardController.java
@@ -3,14 +3,14 @@ package edu.byui.apj.storefront.web.controller;
 import edu.byui.apj.storefront.web.model.TradingCard;
 import edu.byui.apj.storefront.web.service.TradingCardClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Controller
+@RestController
 public class TradingCardController {
     private final TradingCardClientService tradingCardClientService;
 
