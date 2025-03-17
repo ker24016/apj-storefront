93702a74ac58396c391e9adaeaa888088d5950ff
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-17 14:43:55 -0600

    Create tutorial11-1

---
+++ tutorial11-1/src/main/java/edu/byui/apj/storefront/tutorial111/Tutorial111Application.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorial111Application.class, args);
+    }
+
+}
