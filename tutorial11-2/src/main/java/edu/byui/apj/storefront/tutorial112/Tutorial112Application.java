b77fbdf606388a7b3f965f075067ea5415c32c63
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-19 14:47:56 -0600

    tutorial 11-2 basics

--- tutorial11-2/src/main/java/edu/byui/apj/storefront/tutorial112/Tutorial112Application.java
+++ tutorial11-2/src/main/java/edu/byui/apj/storefront/tutorial112/Tutorial112Application.java
@@ -2,8 +2,10 @@ package edu.byui.apj.storefront.tutorial112;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Tutorial112Application {
 
     public static void main(String[] args) {
