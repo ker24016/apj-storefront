daf8a0cd4a9e456b2637360415f1edb1c1b7d705
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-19 14:21:49 -0600

    Init tutorial 11-2

---
+++ tutorial11-2/src/main/java/edu/byui/apj/storefront/tutorial112/Tutorial112Application.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorial112Application.class, args);
+    }
+
+}
