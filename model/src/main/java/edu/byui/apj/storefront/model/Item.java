dbff6b1b8b0193ed77f68f49cb3e00706d68e93e
ker24016 <200663988+ker24016@users.noreply.github.com>
2025-03-21 14:39:42 -0600

    W11 Prove Model

---
+++ model/src/main/java/edu/byui/apj/storefront/model/Item.java
@@ -0,0 +1,18 @@
+package edu.byui.apj.storefront.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+public class Item {
+
+    @EqualsAndHashCode.Include
+    Long id;
+
+    Cart cart;
+
+    String cardId;
+    String name;
+    Double price;
+    Integer quantity;
+}
